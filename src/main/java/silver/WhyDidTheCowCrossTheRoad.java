28e57027a39bc8c6d756e8f509dbf02682750651
Qi Xia <qixia@aaaaaapple.local>
2021-02-27 20:10:48 -0600

    new file added

---
+++ src/main/java/silver/WhyDidTheCowCrossTheRoad.java
@@ -0,0 +1,13 @@
+package silver;
+
+
+import java.io.File;
+import java.util.Scanner;
+
+public class WhyDidTheCowCrossTheRoad {
+    public static void main(String[] args) throws Exception {
+        Scanner stdin = new Scanner(new File("helpcross.in"));
+
+
+    }
+}
